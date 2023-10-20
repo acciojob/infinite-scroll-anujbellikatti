587d406b88f6f981ab4cfabd6167a4f27fbe2fea
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-20 23:57:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-//your code here!
+document.addEventListener("DOMContentLoaded", function() {
+    const list = document.getElementById("infi-list");
 
+    // Function to add more list items
+    function addMoreItems() {
+        const totalItems = list.getElementsByTagName("li").length;
+        const itemsToAdd = 10;
+
+        for (let i = totalItems + 1; i <= totalItems + itemsToAdd; i++) {
+            const listItem = document.createElement("li");
+            listItem.textContent = `List item ${i}`;
+            list.appendChild(listItem);
+        }
+    }
+
+    // Add initial list items
+    addMoreItems();
+
+    // Event listener to detect scroll to the bottom of the list
+    list.addEventListener("scroll", function() {
+        if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+            // User has reached the end of the list, add more items
+            addMoreItems();
+        }
+    });
+});
